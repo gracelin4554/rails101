251a460b9e2c8d794e21903ddecf5129b1819e2d
Jenny <jenny@Jennys-Mac.local>
2019-10-25 23:32:53 -0700

    use simple_format helper to descorate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -31,7 +31,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-     if @group.update(group_params)
+    if @group.update(group_params)
          redirect_to groups_path, notice: "Update Success!"
     else
          render  :edit
